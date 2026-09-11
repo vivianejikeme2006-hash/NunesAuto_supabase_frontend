545cdb6cf585880179a2d6e27306d5e6bac2e132
johnsimao18-eng <johnsimao18@gmail.com>
2026-09-11 10:36:46 +0200

    Mainly worked on the products page and the login and sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React from "react"
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import './App.css'
 import SignUp from "./Components/SignUp"
-import LogInForm from "./Components/LogIn";
+import LogInForm from "./Components/LogIn"
 import Home from "./Components/Home"
 
 function ProtectedRoute({ children }) {
   const user = JSON.parse(localStorage.getItem("user"));
-  return user ? children : <Navigate to="/" replace />;
+  return user ? children : <Navigate to="/login" replace />;
+}
+
+function RootRedirect() {
+  const user = JSON.parse(localStorage.getItem("user"));
+  return user ? <Navigate to="/home" replace /> : <Navigate to="/login" replace />;
 }
 
 function App() {
@@ -15,7 +18,8 @@ function App() {
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<SignUp />} />
+          <Route path="/" element={<RootRedirect />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<LogInForm />} />
           <Route
             path="/home"
@@ -25,7 +29,6 @@ function App() {
               </ProtectedRoute>
             }
           />
-          <Route path="/" element={<Home />} />
           {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
       </div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,29 @@
+import NavBar from "./NavBar";
 import "./Home.css";
 
 function Home() {
   return (
     <div className="homePage">
+      <NavBar />
+
       <section className="hero">
         <div className="heroText">
-          <h1>Find Your Dream<br /><span>Car Part</span></h1>
+          <div className="searchBar">
+            <div className="searchField">Select Car</div>
+            <div className="searchField">Select Part</div>
+            <button className="searchButton">Search Now</button>
+          </div>
+
+          <h1>
+            Find Your Dream<br />
+            <span>Car Part</span>
+          </h1>
           <p>Quality parts, trusted service, and everything Nunes Auto stands for.</p>
           <button className="heroButton">Shop Now</button>
+        </div>
+
+        <div className="heroImage">
+          <img src="/images/hero-car.png" alt="Featured car" />
         </div>
       </section>
 
@@ -15,10 +31,22 @@ function Home() {
         <h2>Categories</h2>
         <p className="categoriesSubtext">Find the car part you are looking for</p>
         <div className="categoryGrid">
-          <div className="categoryCard">Wheels &amp; Tires</div>
-          <div className="categoryCard">Accessories</div>
-          <div className="categoryCard">Engines</div>
-          <div className="categoryCard">Lights</div>
+          <div className="categoryCard">
+            <img src="/images/wheels-tires.jpg" alt="Wheels & Tires" />
+            <span>Wheels &amp; Tires</span>
+          </div>
+          <div className="categoryCard">
+            <img src="/images/accessories.jpg" alt="Accessories" />
+            <span>Accessories</span>
+          </div>
+          <div className="categoryCard">
+            <img src="/images/engines.jpg" alt="Engines" />
+            <span>Engines</span>
+          </div>
+          <div className="categoryCard">
+            <img src="/images/lights.jpg" alt="Lights" />
+            <span>Lights</span>
+          </div>
         </div>
       </section>
     </div>
